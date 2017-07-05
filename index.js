7e71f1de94bf6a4c7fc8c6e22517d707d86a6ad8
Elena Sufieva <fiolent06@gmail.com>
2017-07-05 16:12:51 +0200

    Add custom logs and make webhook smarter

--- index.js
+++ index.js
@@ -6,6 +6,16 @@
 */
 'use strict';
 
+const gcloud = require('gcloud')({
+  keyFilename: './config/key.json',
+  projectId: 'leo-tnoqwg'
+});
+const logging = gcloud.logging();
+const log = logging.log('my-custom-log');
+const entry = log.entry('compute.googleapis.com', {
+  user: 'my_username'
+});
+
 let recipes = {};
 recipes.desserts = require('./recipes/desserts');
 recipes.fish = require('./recipes/fish');
@@ -14,26 +24,46 @@ recipes.meat = require('./recipes/meat');
 
 
 exports.recipeWebhook = (req, res) => {
-  let params = req.body.result.contexts[0]['parameters'],
-  context = req.body.result.contexts[0]['name'];
+  let recipeContext = req.body.result.contexts.find(context => {
+    return context.name == 'recipecontext';
+  });
+  let category = recipeContext.parameters.category,
+  id = recipeContext.parameters.id,
+  intent = req.body.result.metadata.intentName,
+  params = { category, id };
+  //write a custom log for debugging
+  log.debug(entry, function(err, apiResponse) {
+    console.log('this is a custom log');
+    console.log('request intent: ', intent);
+  });
+
   let result;
-  if (context == 'ingredients') {
-    result = getIngredients(params.category, params.id);
-  } else if (context == 'whole_recipe') {
-    result = getFullRecipe(params.category, params.id);
+  switch (intent) {
+    case 'read_ingredients':
+      result = getIngredients(params);
+      break;
+    case 'read_all_steps':
+      result = getFullRecipe(params);
+      break;
+    default:
+      result = 'Sorry, I didn\'t find the intent name.';
   }
 
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ "speech": result, "displayText": result}));
 };
 
-const getIngredients = (category, id) => {
+const getIngredients = (params) => {
+  let category = params.category,
+    id = params.id;
   let recipe = recipes[category][id];
 
   return recipe['ingredients'][0];
 };
 
-const getFullRecipe = (category, id) => {
+const getFullRecipe = (params) => {
+  let category = params.category,
+    id = params.id;
   let fullRecipe = recipes[category][id]['steps'].join(' ');
   return fullRecipe;
 }
